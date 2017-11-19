c8545ef5bc2c69bc2a54be61e30db2909b442c80
gongchen <gongchen@usc.edu>
2017-11-18 16:20:40 -0800

    loop through images

--- project3/src/main.cpp
+++ project3/src/main.cpp
@@ -37,65 +37,70 @@ int main(int argc, char* argv[]) {
     //To learn the naive Bayesian classifier,
     //you should do all the following steps only for the training set.
     int countImagesforEachDigit [] = {0,0,0,0,0,0,0,0,0,0};
-    
     for (int i  = 0; i < trainLabels.size(); i++){
         countImagesforEachDigit[static_cast<int>(testLabels[i])]++;
     }
-//    for (int i = 0; i < 10; i++){
-//        std::cout << i << "  " << countImagesforEachDigit[i] << std::endl;
+    int plClassPixel2DArray [10][783];
+    //Initialize the 2d array
+    for (int i = 0; i < 10; i++){
+        for (int j = 0; j < 783; j++){
+            plClassPixel2DArray [i][j] = 0;
+        }
+    }
+    //Fill the 2d array
+    //Step 0. Iterate over the images
+    //Not range based for loop cuz order matters
+    for (std::vector<std::vector<unsigned char>>::size_type i = 0; i != trainImages.size(); i++) {
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+//    //print out one of the training images
+//    for (int f=0; f<numFeatures; f++) {
+//        // get value of pixel f (0 or 1) from training image trainImageToPrint
+//        int pixelIntValue = static_cast<int>(trainImages[trainImageToPrint][f]);
+//        if (f % width == 0) {
+//            std::cout<<std::endl;
+//        }
+//        std::cout<<pixelIntValue<<" ";
 //    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    //print out one of the training images
-    for (int f=0; f<numFeatures; f++) {
-        // get value of pixel f (0 or 1) from training image trainImageToPrint
-        int pixelIntValue = static_cast<int>(trainImages[trainImageToPrint][f]);
-        if (f % width == 0) {
-            std::cout<<std::endl;
-        }
-        std::cout<<pixelIntValue<<" ";
-    }
-    std::cout<<std::endl;
-    // print the associated label (correct digit) for training image trainImageToPrint
-    std::cout<<"Label: "<<static_cast<int>(trainLabels[trainImageToPrint])<<std::endl;
-    //print out one of the test images
-    for (int f=0; f<numFeatures; f++) {
-        // get value of pixel f (0 or 1) from training image trainImageToPrint
-        int pixelIntValue = static_cast<int>(testImages[testImageToPrint][f]);
-        if (f % width == 0) {
-            std::cout<<std::endl;
-        }
-        std::cout<<pixelIntValue<<" ";
-    }
-    std::cout<<std::endl;
-    // print the associated label (correct digit) for test image testImageToPrint
-    std::cout<<"Label: "<<static_cast<int>(testLabels[testImageToPrint])<<std::endl;
-    std::vector<unsigned char> trainI(numFeatures);
-    std::vector<unsigned char> testI(numFeatures);
-    for (int f=0; f<numFeatures; f++) {
-        int trainV = 255*(static_cast<int>(trainImages[trainImageToPrint][f]));
-        int testV = 255*(static_cast<int>(testImages[testImageToPrint][f]));
-        trainI[f] = static_cast<unsigned char>(trainV);
-        testI[f] = static_cast<unsigned char>(testV);
-    }
-    std::stringstream ssTrain;
-    std::stringstream ssTest;
-    ssTrain << "../output/train" <<trainImageToPrint<<"Label"<<static_cast<int>(trainLabels[trainImageToPrint])<<".bmp";
-    ssTest << "../output/test" <<testImageToPrint<<"Label"<<static_cast<int>(testLabels[testImageToPrint])<<".bmp";
-    Bitmap::writeBitmap(trainI, 28, 28, ssTrain.str(), false);
-    Bitmap::writeBitmap(testI, 28, 28, ssTest.str(), false);
+//    std::cout<<std::endl;
+//    // print the associated label (correct digit) for training image trainImageToPrint
+//    std::cout<<"Label: "<<static_cast<int>(trainLabels[trainImageToPrint])<<std::endl;
+//    //print out one of the test images
+//    for (int f=0; f<numFeatures; f++) {
+//        // get value of pixel f (0 or 1) from training image trainImageToPrint
+//        int pixelIntValue = static_cast<int>(testImages[testImageToPrint][f]);
+//        if (f % width == 0) {
+//            std::cout<<std::endl;
+//        }
+//        std::cout<<pixelIntValue<<" ";
+//    }
+//    std::cout<<std::endl;
+//    // print the associated label (correct digit) for test image testImageToPrint
+//    std::cout<<"Label: "<<static_cast<int>(testLabels[testImageToPrint])<<std::endl;
+//    std::vector<unsigned char> trainI(numFeatures);
+//    std::vector<unsigned char> testI(numFeatures);
+//    for (int f=0; f<numFeatures; f++) {
+//        int trainV = 255*(static_cast<int>(trainImages[trainImageToPrint][f]));
+//        int testV = 255*(static_cast<int>(testImages[testImageToPrint][f]));
+//        trainI[f] = static_cast<unsigned char>(trainV);
+//        testI[f] = static_cast<unsigned char>(testV);
+//    }
+//    std::stringstream ssTrain;
+//    std::stringstream ssTest;
+//    ssTrain << "../output/train" <<trainImageToPrint<<"Label"<<static_cast<int>(trainLabels[trainImageToPrint])<<".bmp";
+//    ssTest << "../output/test" <<testImageToPrint<<"Label"<<static_cast<int>(testLabels[testImageToPrint])<<".bmp";
+//    Bitmap::writeBitmap(trainI, 28, 28, ssTrain.str(), false);
+//    Bitmap::writeBitmap(testI, 28, 28, ssTest.str(), false);
     return 0;
 }
 
